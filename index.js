12940b13d4dc67ea4ecdf11aa253e46e328a9550
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-03-18 22:23:51 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 16
+let num4 = 6
+
+const mod = num3 % num4
+console.log(mod)
+
+const max = Math.max(12, 4, 20, 8, 15)
